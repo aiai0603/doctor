3b316c1b7d73cc94209b081eac28d124fa20bca2
张帅 <1016868503@qq.com>
2021-07-20 19:33:01 +0800

    更新了ui

--- final/pages/resident/residentIndex.vue
+++ final/pages/resident/residentIndex.vue
@@ -1,17 +1,17 @@
 <template>
 	<view class="index">
-		<view class="item" @click="goDetails()">
+		<view class="item  bubble" @click="goDetails()">
 			<view style="width: 200rpx; height: 200rpx;">
-				<image src="../../static/icons/logo1.png" style="width: 100%; height: 100%;"></image>
+				<image src="../../static/icons/py.png" style="width: 100%; height: 100%;"></image>
 			</view>
-			<h2>复诊配药</h2>
+			<view>复诊配药</view>
 		</view>
 		
-		<view class="item" @click="goRecord()">
+		<view class="item  bubble" @click="goRecord()">
 			<view style="width: 200rpx; height: 200rpx;">
-			    <image src="../../static/icons/logo2.png" style="width: 100%; height: 100%;"></image>
+			    <image src="../../static/icons/rc.png" style="width: 100%; height: 100%;"></image>
 		    </view>
-			<h2>配药记录</h2>
+			<view>配药记录</view>
 		</view>
 		
 	</view>
@@ -47,6 +47,37 @@
 </script>
 
 <style>
+	
+	.bubble {
+		position: relative;
+		overflow: hidden;
+	}
+	.bubble:after {
+		content: "cccccc";
+		background: #5c9899;
+		position: absolute;
+		width: 750rpx;
+		height: 750rpx;
+		left: calc(50% - 375rpx);
+		top: calc(50% - 375rpx);
+		opacity: 0;
+		margin: auto;
+		border-radius: 50%;
+		transform: scale(1);
+		transition: all 0.5s ease-in-out;
+	}
+	.bubble:active:after {
+		transform: scale(0);
+		opacity: 1;
+		transition: 0s;
+	}
+	
+	
+	page{
+		background:linear-gradient( #6cacff,#8debff);
+		height:100%;
+		
+	}
 	.index {
 		display: flex;
 		flex-flow: column;
@@ -54,9 +85,20 @@
 	}
 	
 	.item {
+		box-shadow: #cccccc 2rpx 2rpx 10rpx;
+		width: 400rpx;
+		height: 400rpx;
+		background-color: rgba(255,255,255,.4);
+		border-radius: 50rpx;
+		color: white;
+		font-size: 40rpx;
 		margin-top: 30%;
 		display: flex;
 		flex-flow: column;
 		align-items: center;
+		justify-content: center;
+	}
+	.item  view{
+		margin: 10rpx;
 	}
 </style>
